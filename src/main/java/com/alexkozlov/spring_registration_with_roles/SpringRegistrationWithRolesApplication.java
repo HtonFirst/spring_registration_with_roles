5be3fd4c5593d8d5e157cb35e9bce6b9210edd93
HtonFirst <al.kozlov.rus@gmail.com>
2023-09-22 17:29:23 +0300

    init with sdk 11.0.3

---
+++ src/main/java/com/alexkozlov/spring_registration_with_roles/SpringRegistrationWithRolesApplication.java
@@ -0,0 +1,13 @@
+package com.alexkozlov.spring_registration_with_roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistrationWithRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRegistrationWithRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexkozlov/spring_registration_with_roles/SpringRegistrationWithRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexkozlov.spring_registration_with_roles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegistrationWithRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
